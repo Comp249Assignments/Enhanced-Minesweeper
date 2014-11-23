5f776b87cad6cddd0a9e53857884aa7189e1bf66
SeanMarcoux <seanmarcoux@hotmail.com>
2014-11-23 18:20:07 -0500

    Added extra flag images

--- src/Square.java
+++ src/Square.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 
 public class Square  extends JButton{
-	private int mines, number;
+	private int mines, number, flags;
 	private boolean clicked,flagged;
 	private int powerup;
-	private ImageIcon squareImage = new ImageIcon("square.png"), flagImage=new ImageIcon("flag.png");
+	private ImageIcon squareImage = new ImageIcon("square.png");
 	private ImageIcon clickedSquareImage = new ImageIcon("square clicked.png");
 	private ImageIcon[] mineImages = {new ImageIcon("mine.png"), new ImageIcon("mine2.png"), new ImageIcon("mine3.png")};
+	private ImageIcon[] flagImages = {new ImageIcon("flag.png"), new ImageIcon("flag2.png"), new ImageIcon("flag3.png")};
 	
 	//array of all the images for the different numbers. TO DO: Someone please make all these images and have the right dimensions for them (16 px by 16 px)
 	private ImageIcon[] numberImages = {new ImageIcon("1.png"), new ImageIcon("2.png"), new ImageIcon("3.png"), new ImageIcon("4.png"), new ImageIcon("5.png"), new ImageIcon("6.png"), new ImageIcon("7.png"), new ImageIcon("8.png"), new ImageIcon("9.png"), new ImageIcon("10.png"), new ImageIcon("11.png"), new ImageIcon("12.png"), new ImageIcon("13.png"), new ImageIcon("14.png"), new ImageIcon("15.png"), new ImageIcon("16.png"), new ImageIcon("17.png"), new ImageIcon("18.png"), new ImageIcon("19.png"), new ImageIcon("20.png"), new ImageIcon("21.png"), new ImageIcon("22.png"), new ImageIcon("23.png"), new ImageIcon("24.png")};
@@ -16,6 +17,7 @@ public class Square  extends JButton{
 		powerup=0;
 		clicked=false;
 		flagged=false;
+		flags=0;
 		//setFont(new Font("Arial", Font.PLAIN, 5));
 		setIcon(squareImage);
 	}
@@ -62,11 +64,21 @@ public class Square  extends JButton{
 	}
 	
 	public void flag(){
-		flagged=!flagged;
+		if(flags==3){
+			setIcon(squareImage);
+			flagged=false;
+			flags=0;
+		}
+		else{
+			flags++;
+			flagged=true;
+			setIcon(flagImages[flags-1]);
+		}
+		/*flagged=!flagged;
 		if(flagged)
 			setIcon(flagImage);
 		else
-			setIcon(squareImage);
+			setIcon(squareImage);*/
 	}
 	
 	//Method that will be run when a space has been clicked. It will return an integer based on what type of space it is (1 for mine, 2 for space with adjacent mines,

1de98e451d8c15329e6bfe13f3c74302ab158cbc
gadielwiggy <gadielwiggy@gmail.com>
2014-11-23 15:34:27 -0500

    built a basic layout for the score board

--- src/HighScoreBoard.java
+++ src/HighScoreBoard.java
@@ -8,6 +8,10 @@ import javax.swing.*;
 public class HighScoreBoard extends JFrame {
 	private JButton okButton=new JButton("OK"), submitButton=new JButton("Submit");
 	private JPanel p1=new JPanel();
+	private JTextField usersScore=new JTextField();
+	private JLabel messageLabel, enterLabel;
+	private JLabel[] highScores=new JLabel[10];
+	private JLabel[] highScorers=new JLabel[10];
 	
 	
 	public HighScoreBoard(){
@@ -16,22 +20,38 @@ public class HighScoreBoard extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setTitle("High Scores");
 		this.setSize(200,300);
-		//this.setLayout(new GridLayout(3,10));
-		p1.add(okButton);
-		add(BorderLayout.SOUTH,p1);
+		this.setLayout(new BorderLayout());
+		p1.setLayout(new GridLayout(10,1));
+		for(int i=0; i<10;i++){
+			p1.add(highScores[i]=new JLabel((1+i)+"."));
+		add(BorderLayout.WEST,p1);
+		}
+		add(BorderLayout.SOUTH,okButton);
 	}
 	public HighScoreBoard(int score){
 		this.setVisible(true);
 		this.setTitle("Congradulations");
-		this.setLayout(new GridLayout(1,2));
-		this.setLocation(null);
+		this.setLayout(new BorderLayout());
+		p1.setLayout(new GridLayout(3,1));
+		this.setLocationRelativeTo(null);
+		this.setSize(300,150);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		messageLabel=new JLabel("Congradulations you have a high score of:\n " +score,SwingConstants.LEFT);
+		enterLabel=new JLabel("Please enter your name",SwingConstants.LEFT);
+		p1.add(messageLabel);
+		p1.add(enterLabel);
+		p1.add(usersScore);
+		add(BorderLayout.NORTH,p1);
+		add(BorderLayout.SOUTH,submitButton);
+		submitButton.addActionListener(new buttonListener());
+		
 	}
 								
 
 	public class buttonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-		setVisible(false);
+			if(e.getSource()==submitButton)
+			setVisible(false);
 		}
 	}
 
